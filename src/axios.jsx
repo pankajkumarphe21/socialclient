1909b219cda6db5a60512c90309af298d6228dde
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-09-26 00:15:44 +0530

    commit

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance=axios.create({
-    baseURL:process.env.BACKEND_URL || 'http://localhost:8800',
+    baseURL:process.env.REACT_APP_BASE_URL || 'http://localhost:8800',
     withCredentials:true
 });
 
