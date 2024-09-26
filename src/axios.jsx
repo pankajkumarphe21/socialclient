b18987a1e7dc3d3280b79d4038760fa466a1047b
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-09-26 13:14:24 +0530

    fresh1

--- src/axios.jsx
+++ src/axios.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance=axios.create({
     baseURL:"https://socialapi-three.vercel.app/",
+    headers:"Access-Control-Allow-Origin"
 });
 
 export default axiosInstance;
