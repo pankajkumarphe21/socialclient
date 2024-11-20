41f5eb09a98629f8406ee3fba5624107ddd9df21
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-11-20 22:03:23 +0530

    preview of image for post added

--- src/components/createpost/CreatePost.jsx
+++ src/components/createpost/CreatePost.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axiosInstance from '../../axios'
 import './createpost.scss'
 import { useNavigate } from 'react-router-dom';
@@ -7,14 +7,27 @@ import { useSelector } from 'react-redux';
 const CreatePost = () => {
     const [desc,setDesc]=useState('');
     const [file, setFile] = useState(null);
+    const [data,setData]=useState(null);
+    const [filePreview,setFilePreview]=useState(null);
     const username=useSelector(state=>state.user.currentUser.username);
     const handleFileChange = (e) => {
         setFile(e.target.files[0]);
       };
       const formData=new FormData();
-      formData.append('image',file)
-      formData.append('desc',desc)
-      const navigate=useNavigate()
+      formData.append('image',file);
+      formData.append('desc',desc);
+      const navigate=useNavigate();
+      const handlePreview=()=>{
+        if(file.type.startsWith('image/')){
+          const objectUrl=URL.createObjectURL(file);
+          setFilePreview(objectUrl)
+          setData(null)
+        }
+        else{
+          setData('preview for this file type not available')
+          setFilePreview(null)
+        }
+      }
     const handleSubmit=async(e)=>{
         e.preventDefault();
         if(file==undefined){
@@ -26,15 +39,23 @@ const CreatePost = () => {
         } catch (error) {
             console.log(error)
         }
-    }
+    };
   return (
     <div className='createPost'>
         <form encType="multipart/form-data">
             <input type="text" required onChange={(e) => setDesc(e.target.value)} name='desc' placeholder='Enter desc' id='desc'/>
             <input type="file" required onChange={handleFileChange} name="image" id="file" />
+            <div onClick={handlePreview}>Get preview of the image</div>
             <button onClick={handleSubmit}>Create Post</button>
+            {filePreview && (
+          <img
+            src={filePreview}
+            alt="Preview"
+            style={{ maxWidth: '300px', maxHeight: '300px',marginTop:'10px',marginBottom:'30px' }}
+          />
+        )}
+            <p>{data}</p>
         </form>
-        {/* <img src={} alt="" /> */}
     </div>
   )
 }
