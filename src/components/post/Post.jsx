47d7a217ed76842722973f31d09cd009334b19c2
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-11-26 11:37:45 +0530

    like feature added

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -6,10 +6,13 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { useSelector } from 'react-redux';
 import EditIcon from '@mui/icons-material/Edit';
 import { formatDistanceToNow } from "date-fns";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const Post = ({data,handleDelete}) => {
   const [username,setUsername]=useState(null);
   const user=useSelector(state=>state.user.currentUser);
+  const [liked,setLiked]=useState(false);
   const navigate=useNavigate();
   let date=formatDistanceToNow(
     data.date,
@@ -18,24 +21,37 @@ const Post = ({data,handleDelete}) => {
   const handleEdit=()=>{
     navigate(`/post/edit/${data._id}`)
   }
+  const handleLike=async()=>{
+    await axiosInstance.post(`/post/like/${data._id}`);
+    setLiked(!liked)
+  };
+  const isLiked=data.likes.filter((val)=>{
+    return val===user._id;
+  })
   useEffect(()=>{
     const fetchUser=async()=>{
       const user=await axiosInstance.get(`/auth/user/${data.userId}`)
       setUsername(user.data.username)
+    };
+    if(isLiked.length>0){
+      setLiked(true);
     }
     fetchUser()
-  },[username])
+  },[username,isLiked]);
   return (
     <div className='post'>
       <div className="top">
         <Link className='username' to={`/profile/${username}`}>{username}</Link>
         <EditIcon onClick={handleEdit} className='edit'/>
+        {username===user.username && <DeleteIcon onClick={handleDelete}/>}
       </div>
       <hr />
       <img height={'250'} src={`data:image/jpeg;base64,${data.base64Image}`} alt=''></img>
       <p>{data.desc}</p>
       <p>{date} ago</p>
-      {username===user.username && <DeleteIcon onClick={handleDelete}/>}
+      <div onClick={handleLike} className={`like ${liked ? 'yes' : ''}`}>
+        { liked ? <FavoriteIcon/> : <FavoriteBorderIcon/> }
+      </div>
     </div>
   )
 }
